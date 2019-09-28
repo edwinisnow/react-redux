b6d5990bf691d902b42b0a49314cc6ff1b36d34f
edwinisnow <edwin04@gmail.com>
2019-09-28 23:59:58 +0100

    Single Reducer

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 // Action
 function buyCake() {
     return {
@@ -10,10 +11,18 @@ function buyCake() {
     }
 
 }
+function buyIceCreams() {
+    return {
+        type: BUY_ICECREAM,
+        info: 'First redux action'
+    }
+
+}
 // Reducer
 // (prevState, action) => newState
 const initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numOfIceCreams: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -21,6 +30,10 @@ const reducer = (state = initialState, action) => {
         case BUY_CAKE: return {
             ...state,
             numOfCakes: state.numOfCakes - 1
+        }
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCreams: state.numOfIceCreams - 1
         }
         default: return state;
     }
@@ -32,4 +45,6 @@ const unsubscribe = store.subscribe(() => console.log('Updated state', store.get
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCreams())
+store.dispatch(buyIceCreams())
 unsubscribe();
